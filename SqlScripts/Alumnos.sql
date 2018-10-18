95f0d64efeac16501107c6929a02edad8fbd8647
0scar Vc <ovirgen0@ucol.mx>
2018-10-17 23:54:18 -0500

    varchar de apellido y nombre de tabla alumnos cambiada de 255 a 40

--- SqlScripts/Alumnos.sql
+++ SqlScripts/Alumnos.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Alumnos(
 IdAlumno int NOT NULL AUTO_INCREMENT,
-ApellidoAlumno varchar(255) NOT NULL,
-NombreAlumno varchar(255) NOT NULL,
-EmailAlumno varchar(255),
+ApellidoAlumno varchar(40) NOT NULL,
+NombreAlumno varchar(40) NOT NULL,
+EmailAlumno varchar(40),
 NumeroTelefonoAlumno int,
 PRIMARY KEY(IdAlumno)
 );
