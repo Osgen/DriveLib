1951d0ebe3eae7ecd443a55b9d0840ff159cfb67
0scar Vc <ovirgen0@ucol.mx>
2018-11-25 13:48:20 -0600

    Author Modelo-Controlador agregado

---
+++ PI/src/main/java/driveLib/Models/Author.java
@@ -0,0 +1,43 @@
+package driveLib.Models;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer idAuthor;
+    private String nameAuthor;
+
+    public Author(){}
+
+    public Author(String nameAuthor){
+        this.nameAuthor = nameAuthor;
+    }
+
+
+    public Integer getIdAuthor() {
+        return idAuthor;
+    }
+
+    public void setIdAuthor(Integer idAuthor) {
+        this.idAuthor = idAuthor;
+    }
+
+    public String getNameAuthor() {
+        return nameAuthor;
+    }
+
+    public void setNameAuthor(String nameAuthor) {
+        this.nameAuthor = nameAuthor;
+    }
+
+
+
+
+}
---
+++ PI/src/main/java/driveLib/Repositories/AuthorRepository.java
@@ -0,0 +1,9 @@
+package driveLib.Repositories;
+
+import driveLib.Models.Author;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends CrudRepository<Author, Integer> {
+}
