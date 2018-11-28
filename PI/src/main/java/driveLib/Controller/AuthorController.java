5b784fa19a55501146cb34ce7cd76b7f8fc3083b
0scar Vc <ovirgen0@ucol.mx>
2018-11-27 20:32:34 -0600

    CRUD author

--- PI/src/main/java/driveLib/Controller/AuthorController.java
+++ PI/src/main/java/driveLib/Controller/AuthorController.java
@@ -6,27 +6,43 @@ import driveLib.Repositories.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
-@RequestMapping("/author")
+@RequestMapping("/authors")
 public class AuthorController {
 
     @Autowired
     AuthorRepository authorRepository;
 
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody String addNewAuthor(@RequestBody Author author){
+    public @ResponseBody Author addNewAuthor(@RequestBody Author author){
         authorRepository.save(author);
-        return "Saved";
+        return author;
     }
 
-    @RequestMapping("/all")
-    public @ResponseBody Iterable<Author> getAllAuthors(){
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Author> getStaffById(@PathVariable("id") int id){
+        return authorRepository.findById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public @ResponseBody Iterable<Author> getAllStaffs(){
         return authorRepository.findAll();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public  @ResponseBody String deleteStaffById(@PathVariable("id") int id){
+        authorRepository.deleteById(id);
+        return "Staff con id: " +id+" eliminado";
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody Author updateStudent(@RequestBody Author student){
+        authorRepository.save(student);
+        return student;
     }
 
 }
