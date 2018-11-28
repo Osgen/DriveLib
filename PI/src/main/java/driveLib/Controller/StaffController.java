50b4873742069d914430d924213f17cc72593b35
0scar Vc <ovirgen0@ucol.mx>
2018-11-27 20:26:14 -0600

    CRUD Staff

--- PI/src/main/java/driveLib/Controller/StaffController.java
+++ PI/src/main/java/driveLib/Controller/StaffController.java
@@ -6,26 +6,42 @@ import driveLib.Repositories.StaffRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
-@RequestMapping("/staff")
+@RequestMapping("/staffs")
 public class StaffController {
 
     @Autowired
     StaffRepository staffRepository;
 
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody String addNewStaff(@RequestBody Staff staff){
+    public @ResponseBody Staff addNewStaff(@RequestBody Staff staff){
         staffRepository.save(staff);
-        return "Saved";
+        return staff;
     }
 
-    @RequestMapping("/all")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Staff> getStaffById(@PathVariable("id") int id){
+        return staffRepository.findById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
     public @ResponseBody Iterable<Staff> getAllStaffs(){
         return staffRepository.findAll();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public  @ResponseBody String deleteStaffById(@PathVariable("id") int id){
+        staffRepository.deleteById(id);
+        return "Staff con id: " +id+" eliminado";
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody Staff updateStudent(@RequestBody Staff student){
+        staffRepository.save(student);
+        return student;
     }
 }
